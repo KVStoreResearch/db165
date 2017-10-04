5f46aa8ba894642110eb279319a37fbc0b22e614
Zachry Bai <zachrybai@gmail.com>
2017-10-04 03:08:38 +0000

    Add functionality to client_context
    * implement function to lookup column from full name
    * implement function to lookup handle from handle name

--- src/client_context.c
+++ src/client_context.c
@@ -1,9 +1,17 @@
+#define _DEFAULT_SOURCE
 #include <string.h>
 
 #include "client_context.h"
 
+/*
+ * lookup_table(char* name)
+ * Returns pointer to a Table given fully qualified column name
+ */
 Table* lookup_table(char *name) {
-	strsep(&name, "."); // advance name to just the table_name 
+	char* db_name = strsep(&name, "."); // advance name to just the table_name 
+	if (strcmp(db_name, current_db->name) != 0)
+		return NULL;
+
 	for (size_t i = 0; i < current_db->tables_size; i++) {
 		if (strcmp(current_db->tables[i].name, name) == 0) {
 			return &current_db->tables[i];
@@ -12,22 +20,62 @@ Table* lookup_table(char *name) {
 	return NULL;
 }
 
-// TODO
+/*
+ * lookup_column(char* name)
+ * Returns pointer to a Column given fully qualified column name
+ */
 Column* lookup_column(char* name) {
+	char* db_name = strsep(&name, ".");
+	char* table_name = strsep(&name, ".");
+	char* db_table_name = (char*) malloc(strlen(db_name) + strlen(table_name) + 1);
+	strcpy(db_table_name, db_name);
+	strcat(db_table_name, ".");
+	strcat(db_table_name, table_name);
+
+	Table* table = lookup_table(db_table_name);
+	if (!table || !name) 
+		return NULL;
+	
+	for (size_t i = 0; i < table->col_count; i++) {
+		if (strcmp(table->columns[i].name, name) == 0) {
+			return &table->columns[i];
+		}
+	}
+
 	return NULL;
 }
 
-int add_handle(char* handle_name, ClientContext* context, bool result) {
+/*
+ * lookup_client_handle(char* handle_name)
+ * Returns pointer to a Handle given fully qualified column name
+ */
+GeneralizedColumnHandle* lookup_client_handle(ClientContext* context, char* handle) {
+	for (int i = 0; i < context->chandles_in_use; i++) {
+		if (strcmp(context->chandle_table[i].name, handle) == 0) {
+			return &context->chandle_table[i];
+		}
+	}
+	return NULL;
+}
+
+/*
+ * add_handle(ClientContext* context, char* handle, bool result)
+ * Returns 0 on success, -1 on failure
+ */
+int add_handle(ClientContext* context, char* handle, bool result) {
 	if (context->chandles_in_use == context->chandle_slots) {
 		// cannot attach another handle
 		return -1;
 	}
 
 	GeneralizedColumnHandle new_handle;
-	strncpy(new_handle.name, handle_name, strlen(handle_name));
+	for (int i = 0; i < HANDLE_MAX_SIZE; i++) {
+		new_handle.name[i] = '\0';
+	}
+	strncpy(new_handle.name, handle, strlen(handle));
 	new_handle.generalized_column.column_type = result ? RESULT : COLUMN; 
-	int next_slot = context->chandles_in_use;
-	context->chandle_table[next_slot] = new_handle;
+	context->chandle_table[context->chandles_in_use] = new_handle;
+	context->chandles_in_use++;
 
 	return 0;
 }
